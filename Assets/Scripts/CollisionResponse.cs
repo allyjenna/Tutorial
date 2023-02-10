bc60aaa8ddb21e485013f2025c0f9ab882d25672
allyjenna <allyjenna@me.com>
2023-02-10 12:31:26 -0500

    finished tutorial 4

---
+++ Assets/Scripts/CollisionResponse.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionResponse : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //when a collider enters a collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject == player)
+        {
+            Vector3 force = this.transform.position - collision.collider.gameObject.transform.position;
+            force = force * 200f;
+            this.GetComponent<Rigidbody>().AddForce(force);
+        }
+    }
+}
---
+++ Assets/Scripts/PickerUpper.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickerUpper : MonoBehaviour
+{
+    public int count = 0;
+    public GameObject particlePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Pickup"))
+        {
+            GameObject.Instantiate(particlePrefab, collision.collider.gameObject.transform.position, Quaternion.identity); 
+            GameObject.Destroy(collision.collider.gameObject);
+
+            count = count + 1;
+
+            this.GetComponent<AudioSource>().Play();
+
+            Debug.Log("Pickup counter: " + count);
+        }
+    }
+}
